814dffe730129eb665ccd0b65c28832f9ecf0037
Darren640 <darrenlopez956@gmail.com>
2026-03-04 04:28:27 +0000

    RGV-Health-survey

---
+++ rgv-health-survey/server.js
@@ -0,0 +1,35 @@
+console.log("SERVER STARTED");
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = 5000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/submit", (req, res)  => {
+    const newResponse = req.body;
+
+    const filePath = path.join(__dirname, "responses.json");
+
+    let responses = [];
+
+    if (fs.existsSync(filePath)) {
+        const data = fs.readFileSync(filePath);
+        responses = JSON.parse(data);
+    }
+
+    responses.push(newResponse);
+
+    fs.writeFileSync(filePath, JSON.stringify(responses, null, 2));
+
+    res.redirect("/thankyou.html");
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
